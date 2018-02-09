319559cee1816d8511eece7d35170f900a7415e7
Ari <ariyana.pilskalns@umontana.edu>
2018-02-08 23:59:18 -0700

    week 3.01

--- course-work /week-3/02_hello-world/sketch.js
+++ course-work /week-3/02_hello-world/sketch.js
@@ -1,7 +1,21 @@
+/*
+name: sketch.js
+last modified: 2018-02-08
+
+*/
+
+
+// this is an in line comment
+// this is another comment.
+
+// this is the setup() function definition
+// this is required by p5 for setup
 function setup(){
-  
+
 }
 
+// This is the draw() funtion defintion
+// this gets called after setup() finishes executing
 function draw(){
   ellipse(20, 20, 30, 30 );
 }
---
+++ course-work /week-3/04_canvas/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+
+createCanvas( 600, 400, );
+
+// background( 255, 204, 0);
+ // background('black')
+// background('red')
+// background('green')
+// background('beige')
+// background('yellow')
+// background('brown')
+// background('magenta')
+// background('pink')
+// background('orange')
+  // NOTE: Be sure to keep the word surrounded with quotes.
+}
