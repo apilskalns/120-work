16804fa49ddbf0244d3727695083068d5d8eec2e
Ari <ariyana.pilskalns@umontana.edu>
2018-02-09 00:22:54 -0700

    Hello World

--- course-work /week-3/04_canvas/sketch.js
+++ course-work /week-3/04_canvas/sketch.js
@@ -2,7 +2,9 @@ function setup() {
 
 createCanvas( 600, 400, );
 
-// background( 255, 204, 0);
+
+ background( 255, 204, 0);
+ 
  // background('black')
 // background('red')
 // background('green')
---
+++ hw-3/sketch.js
@@ -0,0 +1,13 @@
+// setup function
+function setup() {
+
+    createCanvas( 600, 400 );
+
+    background( 'purple' );
+}
+
+// draw function
+function draw() {
+
+    text( "Hello World!", 20, 30 );
+}
